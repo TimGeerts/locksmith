18a0e8754f080b75f233bf6281ec687140ab0c46
Tim Geerts <timgeerts@Tims-MacBook-Air.local>
2020-12-25 16:57:30 +0100

    fix(keys): fix dungeon acronyms

--- src/modules/key.ts
+++ src/modules/key.ts
@@ -1,13 +1,13 @@
-import { MessageEmbed, Message, User, EmbedField } from 'discord.js';
-import { Command, CommandMessage, Description, Client } from '@typeit/discord';
-import { getDungeons } from '../services/resource.service';
-import { IDungeon } from '../types';
-import { Utils } from '../utils';
+import { MessageEmbed, Message, User, EmbedField } from "discord.js";
+import { Command, CommandMessage, Description, Client } from "@typeit/discord";
+import { getDungeons } from "../services/resource.service";
+import { IDungeon } from "../types";
+import { Utils } from "../utils";
 
 export abstract class Key {
   private client: Client;
 
-  @Command('key :key :level :tank :heal :dps')
+  @Command("key :key :level :tank :heal :dps")
   @Description("Displays a template for people to 'sign up' for a given key")
   async key(command: CommandMessage, client: Client) {
     this.client = client;
@@ -18,32 +18,40 @@ export abstract class Key {
           let keyEmbed = new MessageEmbed();
           if (!command.args.key && !command.args.level) {
             helpEmbed
-              .setColor('#007bff')
-              .setTitle('Usage')
-              .setDescription('Some example usages of the `?key` command')
+              .setColor("#007bff")
+              .setTitle("Usage")
+              .setDescription("Some example usages of the `?key` command")
               .addField(
-                'Syntax',
-                '`?key <dungeon> <level> <tank> <healer> <dps>`\n*(tank/healer/dps are optional parameters)*'
+                "Syntax",
+                "`?key <dungeon> <level> <tank> <healer> <dps>`\n*(tank/healer/dps are optional parameters)*"
               )
-              .addField('Looking for a full group', '`?key AD 18`')
-              .addField('Looking for two dps', '`?key AD 18 0 0 2`')
-              .addField('Looking for tank and healer', '`?key AD 18 1 1 0`');
+              .addField("Looking for a full group", "`?key MoTS 18`")
+              .addField("Looking for two dps", "`?key HoA 18 0 0 2`")
+              .addField("Looking for tank and healer", "`?key SoA 18 1 1 0`");
             const dungeon_acronyms = dungeons
               .sort((a, b) => (a.name > b.name ? 1 : -1))
               .map((d) => `${d.name}: \`${d.tags[0]}\``);
-            helpEmbed.addField('Dungeon acronyms', dungeon_acronyms.join('\n'));
+            helpEmbed.addField("Dungeon acronyms", dungeon_acronyms.join("\n"));
 
             command.reply(helpEmbed);
           } else {
             const key = command.args.key;
             const level = command.args.level;
             const missingRoles = this.findMissingRoles(command.args);
-            const dungeon = dungeons.find((d) => d.tags.map((t) => t.toLowerCase()).indexOf(key?.toLowerCase()) > -1);
+            const dungeon = dungeons.find(
+              (d) =>
+                d.tags.map((t) => t.toLowerCase()).indexOf(key?.toLowerCase()) >
+                -1
+            );
             if (!dungeon) {
-              throw new Error(`No dungeon was found for the parameter  \`${key}\``);
+              throw new Error(
+                `No dungeon was found for the parameter  \`${key}\``
+              );
             }
             if (!level || isNaN(level)) {
-              throw new Error(`No keylevel could be determined from the parameter \`${level}\``);
+              throw new Error(
+                `No keylevel could be determined from the parameter \`${level}\``
+              );
             }
             command.args.key = dungeon.name;
             keyEmbed = this.createEmbed(command);
@@ -56,16 +64,18 @@ export abstract class Key {
               missingRoles.forEach((r) => {
                 m.react(Utils.getEmojiForReaction(r));
               });
-              m.react('🔒');
+              m.react("🔒");
               this.followReactions(m, keyEmbed);
             });
           }
         } else {
-          throw new Error('No dungeons were found');
+          throw new Error("No dungeons were found");
         }
       })
       .catch((err: Error) => {
-        command.reply(`Sorry, I had some trouble fetching that information.\n\n${err.message}`);
+        command.reply(
+          `Sorry, I had some trouble fetching that information.\n\n${err.message}`
+        );
       });
   }
 
@@ -73,13 +83,13 @@ export abstract class Key {
   private findMissingRoles(args: any): string[] {
     let arr: string[] = [];
     if (args.tank === 1 || args.tank === undefined) {
-      arr.push('Tank');
+      arr.push("Tank");
     }
     if (args.heal === 1 || args.heal === undefined) {
-      arr.push('Healer');
+      arr.push("Healer");
     }
     if (args.dps !== 0 || args.dps === undefined) {
-      arr.push('Dps');
+      arr.push("Dps");
     }
     return arr;
   }
@@ -87,9 +97,9 @@ export abstract class Key {
   // creates the reaction handlers ("on" and "remove")
   private followReactions(msg: Message, embed: MessageEmbed): void {
     const roleCollector = Utils.createRoleReactionCollector(msg);
-    roleCollector.on('collect', (reaction, user) => {
+    roleCollector.on("collect", (reaction, user) => {
       // check if the reaction was the "lock" icon
-      if (reaction.emoji.name === '🔒') {
+      if (reaction.emoji.name === "🔒") {
         if (user.id !== msg.author.id) {
           // ignore and remove the reaction
           reaction.users.remove(user);
@@ -106,7 +116,7 @@ export abstract class Key {
         }
       }
     });
-    roleCollector.on('remove', (reaction, user) => {
+    roleCollector.on("remove", (reaction, user) => {
       const roleToRemove = Utils.findRoleNameForReaction(reaction);
       if (roleToRemove) {
         this.updateEmbed(embed, user, roleToRemove, false);
@@ -122,34 +132,41 @@ export abstract class Key {
     const tank = command.args.tank;
     const heal = command.args.heal;
     let dps = command.args.dps;
-    const embed = new MessageEmbed().setColor('#e6cc80');
+    const embed = new MessageEmbed().setColor("#e6cc80");
     embed.setTitle(`[LFG] ${key} +${level}`);
     if (tank === 1 || tank === undefined) {
-      embed.addField(Utils.getEmoji('Tank'), '...');
+      embed.addField(Utils.getEmoji("Tank"), "...");
     }
     if (heal === 1 || heal === undefined) {
-      embed.addField(Utils.getEmoji('Healer'), '...');
+      embed.addField(Utils.getEmoji("Healer"), "...");
     }
     dps = dps === undefined || dps > 3 ? 3 : dps;
     for (let i = 0; i < dps; i++) {
-      embed.addField(Utils.getEmoji('Dps'), '...');
+      embed.addField(Utils.getEmoji("Dps"), "...");
     }
     return embed;
   }
 
   // update existing embed (following a reaction change)
-  private updateEmbed(embed: MessageEmbed, user: User, role: string, add = true): void {
+  private updateEmbed(
+    embed: MessageEmbed,
+    user: User,
+    role: string,
+    add = true
+  ): void {
     if (!embed?.fields) return;
     const userTag = `<@${user.id}>`;
     // determine if the current user has already signed (only used for 'add' action)
     const signed = embed.fields.some((f) => f.value === userTag);
     // determine the fields we need based on the role/emoji
-    const fields: EmbedField[] = embed.fields.filter((f) => f.name === Utils.getEmoji(role));
+    const fields: EmbedField[] = embed.fields.filter(
+      (f) => f.name === Utils.getEmoji(role)
+    );
 
     // if the reaction is to "add" an unexisting signup
     if (add && !signed) {
       // find the first empty field
-      let empField = fields.find((f) => f.value === '...');
+      let empField = fields.find((f) => f.value === "...");
       if (empField) {
         empField.value = userTag;
       }
@@ -159,7 +176,7 @@ export abstract class Key {
       // find the first field containing the user (there should actually only be one)
       let userField = fields.find((f) => f.value === userTag);
       if (userField) {
-        userField.value = '...';
+        userField.value = "...";
       }
     }
   }
@@ -167,9 +184,9 @@ export abstract class Key {
   // close the embed message
   private closeEmbed(embed: MessageEmbed) {
     embed
-      .setColor('#000')
-      .setTitle(embed.title.replace('[LFG]', '[FULL]'))
-      .setDescription('*Signups are closed*')
-      .setFooter('Next time, be quicker to join a key, pleb!');
+      .setColor("#000")
+      .setTitle(embed.title.replace("[LFG]", "[FULL]"))
+      .setDescription("*Signups are closed*")
+      .setFooter("Next time, be quicker to join a key, pleb!");
   }
 }
